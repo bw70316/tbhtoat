98de02b206bfa550aae6a688f4b277ecfac971d5
Brian <bw70316@gmail.com>
2018-05-28 12:44:26 -0400

    heroku deploy

--- config/database.php
+++ config/database.php
@@ -1,10 +1,6 @@
 <?php
 
-$url = parse_url(getenv("DATABASE_URL"));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+
 
 return [
 
@@ -84,18 +80,7 @@ return [
             'schema' => 'public',
         ],
 
-        'pgsql_production' => [
-            'driver' => 'pgsql',
-            'host' => '$host',
-            'database' => '$database',
-            'username' => '$username',
-            'password' => '$password',
-            'charset'=> 'utf8',
-            'prefix' => '',
-            'schema'=> 'public',
-            'sslmode' => 'prefer',
-
-        ]
+     
     ],
 
     /*
